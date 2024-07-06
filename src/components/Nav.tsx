0b8b726f83d23d117492afb83b8f58648ef5251c
Favour Emmanuel <iyenefavour@gmail.com>
2024-07-06 17:18:18 +0100

    finished designing the nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,9 +3,13 @@ import { navItems } from "../constants";
 import { Icon } from "@iconify/react/dist/iconify.js";
 
 const Nav = () => {
+  const [nav, setNav] = useState(false);
+  const toggleNav = () => {
+    setNav(!nav);
+  };
   return (
-    <nav className="sticky top-0 z-50 py-3 border-b border-neutral-700/80 backdrop-blur-lg bg-appBlack text-white">
-      <div className="container px-10 mx-auto text-sm relative">
+    <nav className="sticky top-0 z-50 py-3.5 border-b border-neutral-700/80 backdrop-blur-lg bg-appBlack text-white">
+      <div className="container px-8 mx-auto text-sm relative">
         <div className="flex items-center justify-between">
           <h1 className="text-xl font-bold tracking-tight">Techtrove</h1>
           <ul className="hidden lg:flex ml-14 space-x-12">
@@ -16,20 +20,44 @@ const Nav = () => {
             ))}
           </ul>
           <div className="hidden lg:flex justify-center items-center space-x-8">
-            <span className="text-2xl">
+            <span className="text-2xl cursor-pointer">
               <Icon icon="mdi:cart" />
             </span>
             <a href="#" className="py-2 px-3 border rounded-md">
               Sign In
             </a>
           </div>
-          <div>
-            <span className="text-2xl">
-              <Icon icon="mingcute:menu-line" />
-            </span>
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button onClick={toggleNav}>
+              {nav ? (
+                <span className="text-2xl transition-transform">
+                  <Icon icon="mingcute:close-line" />
+                </span>
+              ) : (
+                <span className="text-2xl transition-transform">
+                  <Icon icon="mingcute:menu-line" />
+                </span>
+              )}
+            </button>
           </div>
         </div>
       </div>
+      {nav && (
+        <div className="lg:hidden fixed duration-300 ease-in-out right-0 top-[3.6rem]  z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center">
+          <ul>
+            {navItems.map((item, index) => (
+              <li key={index} className="py-4">
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="flex space-x-5">
+            <a href="#" className="py-2 px-3 border rounded-md">
+              Sign In
+            </a>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
